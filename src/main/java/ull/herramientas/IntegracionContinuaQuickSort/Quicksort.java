0b9b128d4bbfd1de9e4c75a85c3d060792737a88
Orlandy Ariel <alu0100773408@ull.edu.es>
2016-12-21 16:22:42 +0000

    documentación

--- src/main/java/ull/herramientas/IntegracionContinuaQuickSort/Quicksort.java
+++ src/main/java/ull/herramientas/IntegracionContinuaQuickSort/Quicksort.java
@@ -1,12 +1,21 @@
 package ull.herramientas.IntegracionContinuaQuickSort;
-
+/**
+ * Clase Quicksort
+ * \breif esta clase utiliza el algoritmo Quicksort para ordenar arrays de números enteros.
+ * @author Orlandy Ariel Sánchez A.
+ *
+ */
 public class Quicksort  {
 
 	private int[] numbers;
-
 	private int number;
+	
+	/**
+	 * \brief Método que ordena un array de enteros mediante el uso del algoritmo QuickSort
+	 * \param values, Array que se pretende ordenar.
+	 */
 	public void sort(int[] values) {
-		// Check for empty or null array
+		///Comprueba si el array es nulo o está vacio.
 		if (values ==null || values.length==0){
 			return;
 		}
@@ -14,43 +23,44 @@ public class Quicksort  {
 		number = values.length;
 		quicksort(0, number - 1);
 	}
-
+	
+	/**
+	 * \brief Método que contiene el algoritmo Quicksort,
+	 * este ordena valores enteros del array.
+	 * \param low, inicio de subarray que se va a ordenar
+	 * \param high, final del subarray que se va a ordenar.
+	 */
 	private void quicksort(int low, int high) {
 
 		int i = low, j = high;
 		// Get the pivot element from the middle of the list
 		int pivot = numbers[low + (high-low) / 2];
-		// Divide into two lists
+		//Divide la lista en 2.
 		while (i <= j) {
-
-			// If the current value from the left list is smaller then the pivot
-			// element then get the next element from the left list
-
 			while (numbers[i] < pivot) {
 				i++;
 			}
-			// If the current value from the right list is larger then the pivot
-			// element then get the next element from the right list
 			while (numbers[j] > pivot) {
 				j--;
 			}
-			// If we have found a values in the left list which is larger then
-			// the pivot element and if we have found a value in the right list
-			// which is smaller then the pivot element then we exchange the
-			// values.
-			// As we are done we can increase i and j
 			if (i <= j) {
 				exchange(i, j);
 				i++;
 				j--;
 			}
 		}
-		// Recursion
+		/// Recursividad.
 		if (low < j)
 			quicksort(low, j);
 		if (i < high)
 			quicksort(i, high);
 	}
+	/**
+	 * \brief Método que intercambia los datos que se encuentran en las posiciones i y j del array.
+	 * @param i, posición i
+	 * @param j, posición j.
+	 */
+	
 	private void exchange(int i, int j) {
 		int temp = numbers[i];
 		numbers[i] = numbers[j];
