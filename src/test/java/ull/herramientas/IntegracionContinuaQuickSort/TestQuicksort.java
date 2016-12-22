56fa45bf6fc32452e7f299cfac12419f24a89e6c
Orlandy Ariel <alu0100773408@ull.edu.es>
2016-12-22 20:35:39 +0000

    eliminada la dependencia de junit3, añadida doc a los test.

--- src/test/java/ull/herramientas/IntegracionContinuaQuickSort/TestQuicksort.java
+++ src/test/java/ull/herramientas/IntegracionContinuaQuickSort/TestQuicksort.java
@@ -4,31 +4,52 @@ import static org.junit.Assert.*;
 
 import org.junit.Before;
 import org.junit.Test;
-
+/**
+ * \class TestQuicksort
+ * Clase para probar el código, utiliza JUnit 4.11
+ * \author Orlandy Ariel Sánchez A.
+ *
+ */
 public class TestQuicksort
 {
 	private  int [] arrayDesordenado;
 	private int [] arrayOrdenado;
 	private Quicksort quick;
+	/**
+	 * Método que inicializa los atributos antes de cada test
+	 */
 	@Before
 	public void inicializacion()
 	{
 		arrayDesordenado = new int[]{30,23,1,34,2,1,4,9,16};
 		arrayOrdenado = new int[]{1,1,2,4,9,16,23,30,34};
 		quick = new Quicksort();
-		
 	}
-
+	/**
+	 * \brief Método que comprueba que son distintos
+	 */
+	@Test
+	public void arraysDistinto()
+	{
+		assertArrayEquals(arrayDesordenado, arrayOrdenado);
+	}
+	/**
+	 * \brief Método que comprueba que son iguales los arrays
+	 * parte de tener dos arrays con los mismos elementos, pero uno de ellos ordenado
+	 * el otro utilizará el quicksort para ordenarlo y comprabar que efectivamente son iguales.
+	 */
 	@Test 
 	public void arraysIgual()
 	{
 		quick.sort(arrayDesordenado);
 		assertArrayEquals(arrayDesordenado, arrayOrdenado);
 	}
+	
 	@Test
-	public void arraysDistinto()
+	public void arrayVacio()
 	{
-		assertArrayEquals(arrayDesordenado, arrayOrdenado);
+		int [] arrayTemp =new int []{};
+		quick.sort(arrayTemp);
+		assertArrayEquals(new int []{}, arrayTemp);
 	}
-
 }
